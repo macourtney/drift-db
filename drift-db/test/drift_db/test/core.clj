6b7e36e19baac5bd5d29c71ac6f691bc9dd98b24
Matt Courtney <macourtney@gmail.com>
2011-11-04 12:22:19 -0400

    Updated the test to test some of the column spec functions.

--- drift-db/test/drift_db/test/core.clj
+++ drift-db/test/drift_db/test/core.clj
@@ -2,12 +2,47 @@
   (:use [drift-db.core])
   (:use [clojure.test]))
 
+(defn assert-column-spec
+  ([column-spec column-type column-name] (assert-column-spec column-spec column-type column-name {}))
+  ([column-spec column-type column-name mods]
+    (is (= column-spec
+          (merge
+            { :spec-type :column
+              :type column-type
+              :name column-name }
+            mods)))))
+
+(defn assert-date-spec [date-spec column-name]
+  (assert-column-spec date-spec :date column-name))
+
 (deftest test-date
-  (is (= (date :test)
-        { :spec-type :column
-          :type :date
-          :name :test }))
-  (is (= (date :test { :fail :fail })
-        { :spec-type :column
-          :type :date
-          :name :test })))
+  (assert-date-spec (date :test) :test)
+  (assert-date-spec (date :test { :fail :fail }) :test))
+
+(defn assert-date-time-spec [date-spec column-name]
+  (assert-column-spec date-spec :date-time column-name))
+
+(deftest test-date-time
+  (assert-date-time-spec (date-time :test) :test)
+  (assert-date-time-spec (date-time :test { :fail :fail }) :test))
+
+(defn update-map 
+  ([map key value]
+    (if (and map key value)
+      (assoc map key value)
+      map))
+  ([map key value & others]
+    (apply update-map (update-map map key value) others)))
+
+(defn assert-integer-spec
+  ([date-spec column-name]
+    (assert-column-spec date-spec :integer column-name))
+  ([date-spec column-name not-null primary-key]
+    (assert-column-spec date-spec :integer column-name (update-map {} :not-null not-null :primary-key primary-key))))
+
+(deftest test-integer
+  (assert-integer-spec (integer :test) :test)
+  (assert-integer-spec (integer :test { :not-null true }) :test true nil)
+  (assert-integer-spec (integer :test { :primary-key true }) :test nil true)
+  (assert-integer-spec (integer :test { :not-null true :primary-key true }) :test true true)
+  (assert-integer-spec (integer :test { :fail :fail }) :test))
