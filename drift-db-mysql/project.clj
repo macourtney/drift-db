92017fcb8c7c31218408fcace863e186922c10d9
Matt Courtney <macourtney@gmail.com>
2011-11-15 17:16:52 -0500

    Updated the versions to 1.0.0 and created a group id for drift-db.

--- drift-db-mysql/project.clj
+++ drift-db-mysql/project.clj
@@ -1,8 +1,8 @@
-(defproject drift-db-mysql "1.0.0-SNAPSHOT"
+(defproject org.drift-db/drift-db-mysql "1.0.0"
   :description "This is the mysql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.0.0"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [drift-db "1.0.0-SNAPSHOT"]])
+                 [org.drift-db/drift-db "1.0.0"]])
--- drift-db/project.clj
+++ drift-db/project.clj
@@ -1,3 +1,3 @@
-(defproject drift-db "1.0.0-SNAPSHOT"
+(defproject org.drift-db/drift-db "1.0.0"
   :description "A database interface for use with drift."
   :dependencies [[org.clojure/clojure "1.2.1"]])
