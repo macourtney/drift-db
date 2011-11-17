81f31a2d1ba8edb8c6b99f273e975dbeb97340be
Matt Courtney <macourtney@gmail.com>
2011-11-17 13:40:04 -0500

    Added the migrate.clj file for use with the drift config file. Also updated the version to 1.0.2-SNAPSHOT.

--- drift-db-h2/project.clj
+++ drift-db-h2/project.clj
@@ -1,8 +1,8 @@
-(defproject org.drift-db/drift-db-h2 "1.0.1"
+(defproject org.drift-db/drift-db-h2 "1.0.2-SNAPSHOT"
   :description "This is the h2 implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.0.0"]
                  [com.h2database/h2 "1.3.157"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.0.1"]])
+                 [org.drift-db/drift-db "1.0.2-SNAPSHOT"]])
---
+++ drift-db/src/drift_db/migrate.clj
@@ -0,0 +1,38 @@
+(ns drift-db.migrate
+  (:require [drift-db.core :as core]))
+
+(def schema-info-table "schema_info")
+(def version-column :version)
+
+(defn
+  version-table-is-empty []
+  (logging/info (str schema-info-table " is empty. Setting the initial version to 0."))
+  (core/insert-into schema-info-table { version-column 0 })
+  0)
+
+(defn
+  version-table-exists []
+  (logging/info (str schema-info-table " exists"))
+  (if-let [version-result-map (first (core/sql-find { :table schema-info-table :limit 1 }))]
+    (get version-result-map version-column)
+    (version-table-is-empty)))
+
+(defn
+  version-table-does-not-exist []
+  (logging/info (str schema-info-table " does not exist. Creating table..."))
+  (core/create-table schema-info-table 
+    (core/integer version-column { :not-null true }))
+  (version-table-is-empty))
+
+(defn 
+#^{:doc "Gets the current db version number. If the schema info table doesn't exist this function creates it. If the 
+schema info table is empty, then it adds a row and sets the version to 0."}
+  current-version []
+  (if (core/table-exists? schema-info-table)
+    (version-table-exists)
+    (version-table-does-not-exist)))
+
+(defn
+#^{:doc "Updates the version number saved in the schema table in the database."}
+  update-version [new-version]
+  (core/update schema-info-table ["true"] { version-column new-version }))
