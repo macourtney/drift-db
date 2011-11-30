dc56d3540e680684fd85d2aec58ce2822f935a85
Matt Courtney <macourtney@gmail.com>
2011-11-30 10:09:57 -0500

    Updated the version to 1.0.6.

--- drift-db/project.clj
+++ drift-db/project.clj
@@ -1,4 +1,4 @@
-(defproject org.drift-db/drift-db "1.0.6-SNAPSHOT"
+(defproject org.drift-db/drift-db "1.0.6"
   :description "A database interface for use with drift."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]])
