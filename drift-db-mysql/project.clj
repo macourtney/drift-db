3fd28b2001d80c3dafbf43549a20c91f0b0eb5f6
Matt Courtney <macourtney@gmail.com>
2012-01-23 16:11:13 -0500

    Updated the drift-db-mysql version to 1.0.8-SNAPSHOT.

--- drift-db-mysql/project.clj
+++ drift-db-mysql/project.clj
@@ -1,8 +1,8 @@
-(defproject org.drift-db/drift-db-mysql "1.0.7"
+(defproject org.drift-db/drift-db-mysql "1.0.8-SNAPSHOT"
   :description "This is the mysql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.0"]
                  [mysql/mysql-connector-java "5.1.18"]
                  [org.clojure/clojure "1.2.1"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.0.7"]])
+                 [org.drift-db/drift-db "1.0.8-SNAPSHOT"]])
