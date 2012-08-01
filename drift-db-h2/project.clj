329ca2709931d278845d585c78e8516161761834
Matt Courtney <macourtney@gmail.com>
2012-08-01 15:56:01 -0400

    Updated the version to 1.1.3-SNAPSHOT.

--- drift-db-h2/project.clj
+++ drift-db-h2/project.clj
@@ -1,8 +1,8 @@
-(defproject org.drift-db/drift-db-h2 "1.1.2"
+(defproject org.drift-db/drift-db-h2 "1.1.3-SNAPSHOT"
   :description "This is the h2 implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.1"]
                  [com.h2database/h2 "1.3.168"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.1.2"]])
+                 [org.drift-db/drift-db "1.1.3-SNAPSHOT"]])
