d7a7059d597c1a39e9edfb605440391f86bfcbf5
Matthew Courtney <macourtney@gmail.com>
2012-11-21 13:43:58 -0500

    Removed clojure references from the project.clj. Updated .gitignore to ignore log files and Juffedit temporary files. Added a root project.clj file.

--- drift-db-postgresql/project.clj
+++ drift-db-postgresql/project.clj
@@ -1,7 +1,6 @@
 (defproject org.drift-db/drift-db-postgresql "1.1.5-SNAPSHOT"
   :description "This is the postgresql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.2"]
-                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.drift-db/drift-db "1.1.5-SNAPSHOT"]
