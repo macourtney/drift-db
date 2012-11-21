90cca4840c08230ff39a328aa67082139653ab76
Matthew Courtney <macourtney@macourtney-Dimension-8400.(none)>
2012-11-21 09:50:10 -0500

    Removed the clojure version reference from the project.clj files.

--- drift-db-h2/project.clj
+++ drift-db-h2/project.clj
@@ -2,7 +2,6 @@
   :description "This is the h2 implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.2"]
                  [com.h2database/h2 "1.3.168"]
-                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.drift-db/drift-db "1.1.4"]])
