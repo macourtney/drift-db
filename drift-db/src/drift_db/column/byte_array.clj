e9fb21f7341c45cc69caa5d627b584988c6cb55a
Matt Courtney <macourtney@gmail.com>
2013-04-01 16:07:07 -0400

    Added the byte array column.

---
+++ drift-db/src/drift_db/column/byte_array.clj
@@ -0,0 +1,37 @@
+(ns drift-db.column.byte-array
+  (:require [clojure.tools.logging :as logging]
+            [drift-db.column.column :as column]
+            [drift-db.spec :as spec]))
+
+(deftype ByteArrayColumn [name]
+  column/Column
+  (type [column]
+    :byte-array)
+
+  (name [column]
+    name)
+
+  (length [column]
+    nil)
+
+  (precision [column]
+    nil)
+
+  (scale [column]
+    nil)
+
+  (nullable? [column]
+    true)
+
+  (primary-key? [column]
+    false)
+
+  (auto-increment? [column]
+    false))
+
+(extend-type ByteArrayColumn
+  spec/Spec
+  (type [column] :column))
+
+(defn create-column
+  ([name] (ByteArrayColumn. name)))
