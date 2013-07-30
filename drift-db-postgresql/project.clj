cc8f9578058fae5d93539c5bd665d98899409e90
Matt Courtney <macourtney@gmail.com>
2013-07-30 14:48:35 -0400

    Updated the version to 1.1.6

--- drift-db-postgresql/project.clj
+++ drift-db-postgresql/project.clj
@@ -1,7 +1,7 @@
-(defproject org.drift-db/drift-db-postgresql "1.1.6-SNAPSHOT"
+(defproject org.drift-db/drift-db-postgresql "1.1.6"
   :description "This is the postgresql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.2"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.1.6-SNAPSHOT"]
+                 [org.drift-db/drift-db "1.1.6"]
                  [postgresql "9.1-901.jdbc4"]])
