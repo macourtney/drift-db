32434cd076def8aec5af59e20fdd677c5c6b91e3
Matt Courtney <macourtney@gmail.com>
2015-01-18 14:40:21 -0800

    Updated the version to 1.1.7

--- drift-db-mysql/project.clj
+++ drift-db-mysql/project.clj
@@ -1,7 +1,7 @@
-(defproject org.drift-db/drift-db-mysql "1.1.7-SNAPSHOT"
+(defproject org.drift-db/drift-db-mysql "1.1.7"
   :description "This is the mysql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.2"]
                  [mysql/mysql-connector-java "5.1.21"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.1.7-SNAPSHOT"]])
+                 [org.drift-db/drift-db "1.1.7"]])
--- drift-db-postgresql/project.clj
+++ drift-db-postgresql/project.clj
@@ -1,7 +1,7 @@
-(defproject org.drift-db/drift-db-postgresql "1.1.7-SNAPSHOT"
+(defproject org.drift-db/drift-db-postgresql "1.1.7"
   :description "This is the postgresql implementation of the drift-db protocol."
   :dependencies [[clojure-tools "1.1.2"]
                  [org.clojure/java.jdbc "0.3.6"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.drift-db/drift-db "1.1.7-SNAPSHOT"]
+                 [org.drift-db/drift-db "1.1.7"]
                  [postgresql "9.1-901.jdbc4"]])
